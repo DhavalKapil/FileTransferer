b861b939a6dae4a9b941735f02ff6dc74f89a645
Dhaval Kapil <dhavalkapil@gmail.com>
2014-05-12 19:31:06 +0530

    Added support for all platforms

--- dhaval/Settings.java
+++ dhaval/Settings.java
@@ -6,9 +6,11 @@
 
 package dhaval;
 
+import java.io.*;
+
 class Settings
-{   final static String packetSendFile = "temp_send\\pack";
-    final static String packetReceiveFile = "temp_receive\\pack";
+{   final static String packetSendFile = "temp_send" + File.separator + "pack";
+    final static String packetReceiveFile = "temp_receive" + File.separator + "pack";
     final static int noOfPackets = 10;
     final static int port = 9999;
 }
